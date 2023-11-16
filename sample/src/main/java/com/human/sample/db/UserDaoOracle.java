160832c5fef90cbacb4b06aa26e66d1f4f3b6dc0
abcd6563 <108656936+abcd6563@users.noreply.github.com>
2023-11-16 12:44:37 +0900

    First commit && login

---
+++ sample/src/main/java/com/human/sample/db/UserDaoOracle.java
@@ -0,0 +1,22 @@
+package com.human.sample.db;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import com.human.sample.entity.User;
+
+@Mapper
+public interface UserDaoOracle {
+	
+	@Select("select * from users where \"uid\"=#{uid}")
+	public User getUser(String uid);
+	
+	
+	//#{uid} --> user.getUid() 메소드를 불러옴
+	@Insert("insert into users values (#{uid},#{pwd},#{uname},#{email},default, default)")
+	public void insertUser(User user);
+	
+	
+
+}
